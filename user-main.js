8ef65c6f377910c61355d724843ca36e5c82b4fd
chuyoriko <chuyoriko@gmail.com>
2025-12-29 03:52:02 -0800

    Create user-main.js

---
+++ user-main.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Flazu Bypass V1
+// @namespace    Bypass Free
+// @version      0.2.0
+// @author       Flazu
+// @description  Auto-handle Luarmor with stealth ad blocking & anti-detection
+// @match        https://*.luarmor.net/*
+// @match        https://*.luarmor.com/*
+// @match        https://*.linkvertise.com/*
+// @match        https://linkvertise.com/*
+// @match        https://work.ink/*
+// @match        https://*.loot-link.com/*
+// @match        https://loot-link.com/*
+// @match        https://*.loot-links.com/*
+// @match        https://loot-links.com/*
+// @match        https://*.lootlink.org/*
+// @match        https://lootlink.org/*
+// @match        https://*.lootlinks.co/*
+// @match        https://lootlinks.co/*
+// @match        https://*.lootdest.info/*
+// @match        https://lootdest.info/*
+// @match        https://*.lootdest.org/*
+// @match        https://lootdest.org/*
+// @match        https://*.lootdest.com/*
+// @match        https://lootdest.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_openInTab
+// @grant        GM_notification
+// @grant        GM_setClipboard
+// @run-at       document-start
+
+// @updateURL    https://github.com/chuyoriko/userscriptfree/refs/heads/main/script.js
+// @require      https://github.com/chuyoriko/userscriptfree/refs/heads/main/script.js
+// ==/UserScript==
+
