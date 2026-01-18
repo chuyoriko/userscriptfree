bb543133930a1467733eafd70cb2b526d3502b59
chuyoriko <chuyoriko@gmail.com>
2026-01-18 00:37:27 -0800

    Update flazu-bypass.user.js

--- flazu-bypass.user.js
+++ flazu-bypass.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Flazu Bypass V1
 // @namespace    Bypass Free
-// @version      0.3.2
+// @version      1.0.0
 // @author       Flazu
 // @description  Auto-handle Luarmor with stealth ad blocking & anti-detection
 // @match        https://*.luarmor.net/*
